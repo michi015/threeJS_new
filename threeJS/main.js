1c53397f9fdeab9b9f8be9ff491b7531c998023f
michi015 <michi.mathis@gmx.at>
2024-04-19 08:17:25 +0200

    tutorial one done

--- threeJS/main.js
+++ threeJS/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css";
+import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 //create a scene
@@ -9,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64); //just shape: radius
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
 });
 const mesh = new THREE.Mesh(geometry, material); //combine geometry and material
 scene.add(mesh);
@@ -22,6 +24,7 @@ const sizes = {
 //light
 const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
 light.position.set(0, 10, 10);
+light.intensity = 200;
 scene.add(light);
 
 //camera
@@ -39,6 +42,7 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 //define how big our canvas is
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2); //smoother edges - default is 1
 renderer.render(scene, camera);
 
 //controls
@@ -69,3 +73,34 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+//animate it with gsap
+//timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+//Mouse animation color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    //lets animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
